479503315346180d72c8b53fb3c4c6ae12177bb5
saulyaguana <saulyaguanacruz@gmail.com>
2025-12-04 13:17:29 -0500

    Creating the method for compute the angle between two vectors

--- fitness_tracker.py
+++ fitness_tracker.py
@@ -19,13 +19,22 @@ class FitnessTracker:
             raise ExceptionError("Could not found the source path, try it again")
         return video
     
-    def get_landmark_point(landmarks, landmark_point, width, height):
+    def get_landmark_point(self, landmarks, landmark_point, width, height):
         x = int(landmarks.landmark[landmark_point].x * width)
         y = int(landmarks.landmark[landmark_point].y * height)
 
         return np.array([x, y])
     
-    
+    def compute_angle(self, v1, v2):
+        # Unit vector.
+        v1u = v1 / np.linalg.norm(v1)
+        # unit vector.
+        v2u = v2 / np.linalg.norm(v2)
+        # Compute the angle between the two unit vectors.
+        angle_deg = np.arccos(np.dot(v1u, v2u)) * 180 / math.pi
+
+        return angle_deg
+
     def pull_ups(self):
         video = self.source_path
         win_name = "Pull-Ups"
